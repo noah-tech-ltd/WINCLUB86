6ef9be6a09034bf6d37c3fd3d940f98becc0ec19
Wilson.Weng <l7960261@gmail.com>
2021-12-13 17:36:12 +0800

    修正首頁 logo link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { IosInstallTutorialComponent } from './ios-install-tutorial/ios-install-tutorial.component';
 import { AndroidInstallTutorialComponent } from './android-install-tutorial/android-install-tutorial.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AndroidInstallTutorialComponent } from './android-install-tutorial/andr
     AndroidInstallTutorialComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
